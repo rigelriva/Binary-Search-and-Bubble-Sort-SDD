146f14faccd787a957daa2f2d6fb828a0d31ea02
rigelriva <153609066+rigelriva@users.noreply.github.com>
2024-03-30 17:18:32 +1100

    binaryandbubble.py

--- binaryandbubble.py
+++ binaryandbubble.py
@@ -64,13 +64,13 @@ def binary_search():
     low_index = 0   # index 0 is the lowest value
     high_index = len(simarray1) - 1    # the length of the array-1 as the high (because array counting starts at 0)
 
-    while low_index <= high_index:
+    while low_index <= high_index:  # sees if the search range is valid during the loop
         mid = low_index + (high_index - low_index) // 2     # finding the middle of array 
         midpoint_value.set(simarray1[mid])  # Update midpoint_value with the current midpoint
         if simarray1[mid] == target:
             answer1.config(text="Your number: {}, is at index {}".format(target, mid), fg="green") # Number found
             return
-        elif target < simarray1[mid]:   #Changing the high or low depending on if the target is greater or lower
+        elif target < simarray1[mid]:   # Changing the high or low depending on if the target is greater or lower
             high_index = mid - 1    
         else:
             low_index = mid + 1
@@ -81,24 +81,24 @@ input_entry = Entry(page2, textvariable=input_var, font=('calibre', 10, 'normal'
 input_entry.pack()  # input box
 
 answer1 = Label(page2, text="", bg='white')
-answer1.pack(pady=20, padx=50)  # the text that will appear
+answer1.pack(pady=20, padx=50)  # the text that will appear, invalid message or says where the number is 
 
 sub_btn = Button(page2, text='Submit', command=binary_search)
 sub_btn.pack()  # submit button, starts the binary search algorithm 
 
 # Bubble Sort (Page3) ------------------------------------------------------------------
 
-import random
+import random  # this will be used for 'random.shuffle' part of the algorithm which lets the shuffle feature work
 from tkinter import *
 
-simarray2 = [32, 42, 54, 12, 19, 23]
+simarray2 = [32, 42, 54, 12, 19, 23]  # unsorted array of simarray1
 
-lb5 = Label(page3, text=simarray2, bg='white', font=style1)
+lb5 = Label(page3, text=simarray2, bg='white', font=style1)  # visually shows simarray2
 lb5.pack()
 
 def bubble():
     index_length = len(simarray2) - 1 
-    sorted = False 
+    sorted = False   # Flag to track whether the array is sorted or not
 
     while not sorted:   # Continue looping until the array is sorted  
         sorted = True  
@@ -113,13 +113,14 @@ def shuffle():
     random.shuffle(simarray2)
     lb5.config(text=simarray2)  # Update the label to display the shuffled array
 
-sub_btn2 = Button(page3, text='Sort', command=bubble, font=style2)  # Button that sorts the array
+sub_btn2 = Button(page3, text='Sort', command=bubble, font=style2)  # Button that sorts the array, sends a command to start def bubble()
 sub_btn2.pack()
 
-btn5 = Button(page3, text="Shuffle", command=shuffle, font=style2)  # Button that shuffles the array
+btn5 = Button(page3, text="Shuffle", command=shuffle, font=style2)  # Button that shuffles the array, sends a comand to start def shuffle()
 btn5.pack()
 
 # --------------------------------------------------------------------
+
 page1.tkraise()
 window.geometry("550x450")
 window.title("Binary Search and Bubble Sort")
